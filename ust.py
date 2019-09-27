3d50d96e7efcdf57d6e43fb393f4b29b29770f5c
FherStk <porrino.fernando@gmail.com>
2019-09-27 15:42:22 +0200

    first modal window

---
+++ ust.py
@@ -0,0 +1,5 @@
+#source: https://www.devdungeon.com/content/dialog-boxes-python
+#dependencies: pip3 install pyautogui
+
+import pyautogui
+pyautogui.confirm('Aquest ordinador s''apagarà automàticament en 5 minuts')  # returns "OK" or "Cancel"
