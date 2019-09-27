0adf2be7f06bf0d75291f4fda0b603e816314cba
FherStk <porrino.fernando@gmail.com>
2019-09-27 16:01:34 +0200

    building timer

--- ust.py
+++ ust.py
@@ -1,5 +1,23 @@
 #source: https://www.devdungeon.com/content/dialog-boxes-python
+#        https://www.geeksforgeeks.org/timer-objects-python/
+
 #dependencies: pip3 install pyautogui
+#documentation: https://pyautogui.readthedocs.io/en/latest/
 
 import pyautogui
-pyautogui.confirm('Aquest ordinador s''apagarà automàticament en 5 minuts')  # returns "OK" or "Cancel"
+import threading
+import datetime
+
+def chek(): 
+    now = datetime.datetime.now()
+    if now.hour == 16 and now.minute == 0:
+        print("Shut down! \n") 
+    else:            
+        timer = threading.Timer(60.0, chek) #every minute
+        timer.start()     
+
+chek()
+#action = pyautogui.confirm(text='Aquest ordinador s''apagarà automàticament en 5 minuts', title='Apagada automàtica', buttons=['Anul·la l''apagada automàtica'])  # returns "OK" or "Cancel"
+#
+#if action != None:
+#    print('Abort')
