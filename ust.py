1d0c5e7822498b62eff31eb7cf1e03ee967edb66
Fher <porrino.fernando@gmail.com>
2019-09-28 12:37:26 +0200

    first working version, changes pending

--- ust.py
+++ ust.py
@@ -8,18 +8,14 @@
 import pyautogui
 import threading
 import datetime
-import sys
 import os
 
 #TODO: the shutdown times should be in a list
 #TODO: config files with schedule times, temeouts, etc.
 now = datetime.datetime.now()
-shd_time = datetime.datetime(now.year, now.month, now.day, 17,55,0)
+shd_time = datetime.datetime(now.year, now.month, now.day, 12,36,0)
 wrn_time = shd_time - datetime.timedelta(minutes=5)
 polling = 5 #the timers will poll every x sencds.
-
-shd_thread = None
-wrn_thread = None
 
 def shutdownTimer():
     now = datetime.datetime.now()
@@ -32,8 +28,9 @@ def shutdownTimer():
     else:            
        shd_timer.start()   
 
+    return shd_timer
 
-def warningTimer(): 
+def warningTimer(shd_timer): 
     now = datetime.datetime.now()
     wrn_timer = threading.Timer(polling, warningTimer) #every minute
 
@@ -41,20 +38,15 @@ def warningTimer():
         action = pyautogui.confirm(text='Aquest ordinador s''apagarà automàticament a les %s' % shd_time.strftime('%H:%M'), title='Apagada automàtica', buttons=['Anul·la l''apagada automàtica'])  # returns "OK" or "Cancel"
         
         if action != None:
-            pyautogui.alert(text='Si us plau, recordi apagar l''ordinador manualment quan acabi de fer-lo servir. Gràcies. ', title='', button='OK')
-            shd_thread._stop()
+            shd_timer.cancel()
+            pyautogui.alert(text='Si us plau, recordi apagar l''ordinador manualment quan acabi de fer-lo servir. Gràcies. ', title='', button='OK')            
 
     else:            
         wrn_timer.start()       
 
 if __name__ == "__main__":
     #TODO: send the shutdown time to the threads, once finished load the next one
-    shd_thread = threading.Thread(target=shutdownTimer)
-    shd_thread.start()
-    shd_thread.join()
+    shd_timer = shutdownTimer()
 
-    wrn_thread = threading.Thread(target=warningTimer)    
-    wrn_thread.start()
-    wrn_thread.join()
-    
-    sys.exit(0)
+    #The main thread must be able to abort the shutdown
+    warningTimer(shd_timer)
