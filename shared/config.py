c4c29a69dbc7914f6d36559e92f76cb17995b185
Fher <porrino.fernando@gmail.com>
2020-05-21 15:05:56 +0200

    .NET Core Scaffold

--- shared/config.py
+++
@@ -1,8 +0,0 @@
-from shared.popup import Popup
-
-class Config():
-    SHUTDOWN_TIMES = [{"time": "16:00:00", "popup": Popup.ABORT}]
-    WARNING_BEFORE_SHUTDOWN = 1 #in minutes    
-    SERVER = "localhost"
-    PORT = 65003
-    TIMEOUT = 5
