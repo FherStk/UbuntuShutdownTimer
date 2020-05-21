014d1654979330c2ec9afb39fcd13f00e5171e5e
Fher <porrino.fernando@gmail.com>
2020-05-21 16:09:03 +0200

    Traditional scaffold

--- core/Client.cs
+++ core/Client.cs
@@ -16,12 +16,53 @@
     along with UST.  If not, see <https://www.gnu.org/licenses/>.
 */ 
 
-using System;
+using System;    
+using System.IO; 
+using System.Text;    
+using System.Net.Sockets;       
 
 namespace UST.Core
 {
-    class Client
-    {
-       
+    class Client: Server
+    {       
+        public Client(Shutdown[] schedule, int port=261186, bool verbose = false): base(schedule, port, verbose){            
+        }
+
+        public override void Start(){
+            //Source: https://www.c-sharpcorner.com/article/getting-started-with-remote-procedure-call/
+            
+            try{     
+                using (var client = new TcpClient()){
+                    if(this.Verbose) Console.WriteLine("Connecting...");  
+
+                    client.Connect("127.0.0.1", this.Port);    
+                    if(this.Verbose) Console.WriteLine("Connected!");    
+                    
+                    if(this.Verbose) Console.WriteLine("Enter the String you want to send:");    
+                    var input = Console.ReadLine();                            
+                    var stream = client.GetStream();    
+                    var enconding = new ASCIIEncoding();    
+                    byte[] message = enconding.GetBytes(input); 
+
+                    if(this.Verbose) Console.WriteLine("Sending...");    
+                    stream.Write(message, 0, message.Length);    
+                    byte[] buffer = new byte[100];    
+                    int length = stream.Read(buffer, 0, 100);    
+
+                    if(this.Verbose){
+                        for (int i = 0; i < length; i++)    
+                        {    
+                            Console.Write(Convert.ToChar(buffer[i]));    
+                        }   
+                    } 
+            
+                    client.Close();    
+                    if(this.Verbose) Console.ReadLine();    
+                }
+            }
+            catch(Exception ex){
+                if(this.Verbose) Console.WriteLine("Error: {0}", ex.StackTrace); 
+            }            
+        }
     }
 }
