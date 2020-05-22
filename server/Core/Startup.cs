af48aef445988a90b0feefb1691d68376278353a
Fher <porrino.fernando@gmail.com>
2020-05-22 19:39:15 +0200

    License

--- server/Core/Startup.cs
+++ server/Core/Startup.cs
@@ -1,4 +1,26 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿/*
+    Copyright © 2020 Fernando Porrino Serrano
+    Third party software licenses: 
+      - gRPC for C# by gRPC.io:  under the Apache-2.0 License (https://github.com/grpc/grpc)
+      - Protobuf by Google Inc.: under the Copyright  License (https://github.com/protocolbuffers/protobuf)
+
+    This file is part of Ubuntu Shutdown Timer (UST from now on).
+
+    UST is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    UST is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with UST.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
